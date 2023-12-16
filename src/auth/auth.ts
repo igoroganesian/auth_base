31f53249b288ccbb9e543eab82840efb45501f89
Igor Oganesian <igor.oganesian@gmail.com>
2023-12-15 19:18:24 -0500

    express-validator + signup route done

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,39 +1,44 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
+import { body, validationResult } from 'express-validator';
 import bcrypt from 'bcryptjs';
 import pool from '../db';
 
 const router = express.Router();
 
-router.post('/signup', async (req, res) => {
-  try {
-    const { username, password, email } = req.body;
-    let errorMessage = '';
+router.post('/signup',
+  [
+    body('username', 'Username is required').notEmpty(),
+    body('email', 'Invalid email').isEmail(),
+    body('password', 'Password must be at least 8 characters long').isLength({ min: 8 })
+  ],
+  async (req: Request, res: Response) => {
+    try {
+      const errors = validationResult(req);
+      if (!errors.isEmpty()) {
+        return res.status(400).json({ errors: errors.array() });
+      }
 
-    if (!username) {
-      errorMessage += 'Username is required. ';
+      const { username, password, email } = req.body;
+
+      const existingUser = await pool.query(
+        `SELECT * FROM users WHERE username = $1 OR email = $2`,
+        [username, email]);
+
+      if (existingUser.rows.length > 0) {
+        return res.status(409).send('User already exists.');
+      }
+
+      const hashedPassword = await bcrypt.hash(password, 12);
+
+      await pool.query(
+        `INSERT INTO users (username, password, email) VALUES ($1, $2, $3)`,
+        [username, hashedPassword, email]);
+
+      res.status(201).send('Signup successful');
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server error');
     }
+  });
 
-    if (!password) {
-      errorMessage += 'Password is required. ';
-    } else if (password.length < 8) {
-      errorMessage += 'Password must be at least 8 characters long. ';
-    }
-
-    if (!email) {
-      errorMessage += 'Email is required. ';
-    } else if (!email.includes('@')) {
-      errorMessage += 'Email is invalid. ';
-    }
-
-    if (errorMessage) {
-      return res.status(400).send(errorMessage.trim());
-    }
-
-    const hashedPassword = await bcrypt.hash(password, 12);
-    // check for existing user /+ insert into db
-    res.status(201).send('Signup successful');
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Server error');
-  }
-});
+export default router;
