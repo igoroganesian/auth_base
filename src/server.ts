75734380b0dbb6e1c8e9a31cc69ca803ed945f8a
Igor Oganesian <igor.oganesian@gmail.com>
2023-12-15 19:40:53 -0500

    express redundancy

--- src/server.ts
+++ src/server.ts
@@ -1,10 +1,9 @@
 import dotenv from 'dotenv';
 dotenv.config();
 
-import express from 'express';
+import app from './app';
 
-const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
   console.log(`Server running on http://localhost:${PORT}`);
